e3347658c8c46c16a272241e5e6b8e4a5c0045c9
Ex1Kx <thenewestekigoh@gmail.com>
2022-02-08 16:27:23 -0500

    [Ex1] Added The Exercise

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Binario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter U Number: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            long bin = 0;
+            const int divisor = 2;
+            long dig = 0;
+
+            for (int contador = numero % divisor, contador2 = 0; numero > 0; numero /= divisor, contador = numero % divisor, contador2++)
+            {
+                dig = contador % divisor;
+                bin += dig * (long)Math.Pow(10, contador2);
+            }
+            Console.WriteLine(bin);
+        }
+    }
+}
